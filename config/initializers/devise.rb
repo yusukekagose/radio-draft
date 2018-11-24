d5324a3cbbeb64f5bf208d56277730700b0d8cd3
kagose yusuke <yusukekagose@icloud.com>
2018-11-24 09:14:08 +0900

    change device.rb singout via to logout on heroku

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -33,7 +33,7 @@ Devise.setup do |config|
 
   config.reset_password_within = 6.hours
 
-  config.sign_out_via = :delete
+  config.sign_out_via = [:get, :delete]
 
   require 'omniauth-facebook'
   config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], scope: 'email', info_fields: 'email, name'
